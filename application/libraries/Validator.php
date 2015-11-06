c1bd415e45b5edf3ad7db29efc4bb5a0e47a975c
linxingchen <linxingchen@baidu.com>
2015-11-06 23:19:33 +0800

    fin validator check

--- application/libraries/Validator.php
+++ application/libraries/Validator.php
@@ -66,6 +66,107 @@ class Validator{
         return false;
     }
 
+    public static function lessThan($value, $reference, $message = '', $filter = FILTER_VALIDATE_INT){
+        if (false !== filter_var($value, $filter) && $value < $reference){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function lessThanOrEqual($value, $reference, $message = '', $filter = FILTER_VALIDATE_INT){
+        if (false !== filter_var($value, $filter) && $value <= $reference){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function isBool($value, $message = ''){
+        if (true === $value || false === $value || 0 === $value
+                || 1 === $value || '0' === $value || '1' === $value){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function isBoolInt($value, $message = ''){
+        $value = filter_var($value, FILTER_VALIDATE_INT);
+        if (1 === $value || 0 === $value){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function arrayHasKey($key, $array, $message = ''){
+        if (isset($array[$key])){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function inArray($needle, $array, $message = ''){
+        if (in_array($needle, $array)){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function arrayCountRange($array, $min, $max, $message = ''){
+        if (is_array($array)){
+            $count = count($array);
+            if ($min <= $count && $count <= $max){
+                return true;
+            }
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function arrayNotEmpty($array, $message = ''){
+        if (is_array($array) && count($array) > 0){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function isEqual($expect, $actual, $message = ''){
+        if ($expect === $actual){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function any($arg1, $arg2, $message = ''){
+        if ($arg1 || $arg2){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function isTrue($arg1, $message = ''){
+        if (true === $arg1){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function isFalse($arg1, $message = ''){
+        if (false === $arg1){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
     public static function isMobile($value, $message =''){
         if(preg_match('/^1[34578][0-9]{9}$/',$value)){
             return true;
@@ -94,6 +195,22 @@ class Validator{
 
     public static function isEmail($value, $message = ''){
         if (false !== filter_var($value, FILTER_VALIDATE_EMAIL)){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function isUrl($value, $message = '', $flag = null){
+        if (false !== filter_var($value, FILTER_VALIDATE_URL, $flag)){
+            return true;
+        }
+        self::setMessage($message);
+        return false;
+    }
+
+    public static function isIp($value, $message = '', $flag = null){
+        if (false !== filter_var($value, FILTER_VALIDATE_URL, $flag)){
             return true;
         }
         self::setMessage($message);
