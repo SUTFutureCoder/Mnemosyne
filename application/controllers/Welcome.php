930c9ac1ea74b0024fb7be7f9a31e81afa7bf3ec
fangxu02 <fangxu02@meituan.com>
2015-12-08 19:21:15 +0800

    smary 修改

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -28,6 +28,11 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
 	public function index()
 	{
 		$this->load->view('welcome_message');
--- application/libraries/Template.php
+++ application/libraries/Template.php
@@ -1,14 +1,9 @@
 <?php
-/**
- * @package Giant.Union_CPS
- * @author houhuiyang@sankuai.com
- * @copyright Sankuai Ltd.
- * @modified 2011-05-16 
- * @todo 模板引入类
- */
+
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 require_once(TempLates); //引入Smarty
 class Template extends Smarty {
+    public $template;
     public function __construct() {
         $this -> template = new Smarty();
         $this -> template_dir = TempLates_Dir;
