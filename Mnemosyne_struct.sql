5aa864cebe124c56ac7fc970271fcbd9503a4647
方旭 <fangxu02@meituan.com>
2016-01-07 00:24:57 +0800

    add alumni form add alumni_page_form

--- Mnemosyne_struct.sql
+++ Mnemosyne_struct.sql
@@ -248,11 +248,34 @@ CREATE TABLE IF NOT EXISTS `alumni` (
   `id`      BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '唯一id',
   `user_id` BIGINT(20) UNSIGNED NOT NULL COMMENT '用户id',
   `title` varchar(32) NOT NULL COMMENT '同学录标题',
-  `cover` varchar(100) NOT NULL  COMMENT '封面图片存放路径',
+  `cover` varchar(100) NOT NULL  DEFAULT '' COMMENT '封面图片存放路径',
   `create_time` int(10) unsigned NOT NULL COMMENT '创建时间',
   `update_time`int(10) NOT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `IDX_title_id` (`title`)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='同学录表';
+
+
+
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `alumni`
+--
+
+CREATE TABLE IF NOT EXISTS `alumni_page` (
+  `id`      BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '唯一id',
+  `user_id` BIGINT(20) UNSIGNED NOT NULL COMMENT '用户id',
+  `to_user` BIGINT(20) UNSIGNED NOT NULL COMMENT '同学录送达用户id',
+  `background_style` tinyint(3) NOT NULL COMMENT '背景风格',
+  `info` varchar(100) NOT NULL  DEFAULT '' COMMENT '以json形式存放一些其他数据',
+  `message` varchar(1024) NOT NULL DEFAULT '' COMMENT '留言 先来1024这么大,不能有再长的留言吧',
+  `create_time` int(10) unsigned NOT NULL COMMENT '创建时间',
+  `update_time`int(10) NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `IDX_to_user` (`to_user`)
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='同学录表';
 
