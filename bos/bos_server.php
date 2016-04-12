98f33af963b8045c901f48a2128ae07c0d8288f0
linxingchen <linxingchen@baidu.com>
2016-04-12 17:39:35 +0800

    fin router of bos_server & update database struct

--- bos/bos_server.php
+++ bos/bos_server.php
@@ -1,13 +1,4 @@
 <?php
-/**
- *
- * 一律使用POST
- *
- * Created by PhpStorm.
- * User: lin
- * Date: 16-3-19
- * Time: 下午3:06
- */
 require 'config.php';
 //载入目录
 Config::definePath();
@@ -18,14 +9,13 @@ spl_autoload_register(function ($class){
 $strType         = $_GET['type'];
 $strFunctionName = $_GET['qt'];
 
-
 //验证函数白名单
 if (!isset(Config::$funcWhiteList[$strType]) || !in_array($strFunctionName, Config::$funcWhiteList[$strType])){
     Response::responseErrorJson('NoSuchFunction', 'The specified type and function does not exist.');
 }
 
 //直接调用函数
-$ret = $strType::$strFunctionName('14604488844', '65895050358e80c543a7677c32fd6ae54abbc0e766e0c42f7457b4b10c5e0cb3');
+$ret = call_user_func_array(array($strType, $strFunctionName), array('14604488844', '65895050358e80c543a7677c32fd6ae54abbc0e766e0c42f7457b4b10c5e0cb3'));
 
 //返回调用结果
 Response::responseResultJson($ret);
--- bos/bos_struct.sql
+++ bos/bos_struct.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2016-04-10 11:50:57
+-- 生成日期: 2016-04-12 17:23:25
 -- 服务器版本: 5.5.47-0ubuntu0.14.04.1
 -- PHP 版本: 5.5.9-1ubuntu4.14
 
@@ -30,16 +30,18 @@ CREATE TABLE IF NOT EXISTS `bos_bucket` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `bucket_id` bigint(20) NOT NULL COMMENT 'uuid',
   `bucket_name` varchar(32) NOT NULL COMMENT 'bucket名',
+  `user_id` bigint(20) NOT NULL,
   `access_key` varchar(1024) NOT NULL COMMENT '读取key',
   `secret_key` varchar(1024) NOT NULL COMMENT '修改key',
-  `bucket_root` varchar(1024) NOT NULL COMMENT '文件root目录',
+  `bucket_root` varchar(1024) NOT NULL COMMENT '文件root目录, BOSPATH/resroot/user_id/bucket_root',
   `enable_host_list` varchar(10240) NOT NULL COMMENT '允许的访问域名，反盗链，使用json存储，空则允许全部域',
   `enable_null_referer` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否允许空来源',
   `is_public` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否公有',
   `key_need` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否特别key验证，如不需要则使用用户key',
   `ctime` bigint(20) NOT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`,`bucket_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='bucket' AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`,`bucket_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='bucket' AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -52,17 +54,34 @@ CREATE TABLE IF NOT EXISTS `bos_object` (
   `object_id` bigint(20) NOT NULL COMMENT '存储对象id',
   `object_index` varchar(1024) NOT NULL COMMENT '获取存储对象的key，用于http访问',
   `name` varchar(1024) NOT NULL COMMENT '存储对象源文件名',
-  `mine` varchar(1024) NOT NULL COMMENT '文件mine信息',
+  `mime` varchar(1024) NOT NULL COMMENT '文件mime信息',
   `sign` varchar(1024) NOT NULL COMMENT '文件签名',
   `user` bigint(20) NOT NULL COMMENT '用户uuid',
-  `bucket` bigint(20) NOT NULL COMMENT '存放bucket id',
+  `private_share_key` char(4) NOT NULL COMMENT '分享码，由BOS提供',
+  `bucket_id` bigint(20) NOT NULL COMMENT '存放bucket id',
   `is_public` tinyint(4) NOT NULL COMMENT '是否为公有',
+  `is_delete` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否被删除',
   `ctime` bigint(20) NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`),
   KEY `object_index` (`object_index`(255)),
-  KEY `bucket` (`bucket`),
+  KEY `bucket` (`bucket_id`),
   KEY `user` (`user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='BOS服务存储对象表' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `bos_tip_off`
+--
+
+CREATE TABLE IF NOT EXISTS `bos_tip_off` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增字段',
+  `object_id` bigint(20) NOT NULL COMMENT '举报文件id',
+  `user_id` bigint(20) NOT NULL COMMENT '举报人id',
+  `description` varchar(1024) NOT NULL COMMENT '原因',
+  PRIMARY KEY (`id`),
+  KEY `object_id` (`object_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='举报表，水表已拆，不收快递' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -73,12 +92,13 @@ CREATE TABLE IF NOT EXISTS `bos_object` (
 CREATE TABLE IF NOT EXISTS `bos_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `user_name` varchar(32) NOT NULL COMMENT '用户名',
   `access_key` varchar(1024) NOT NULL COMMENT '访问私有资源key',
   `secret_key` varchar(1024) NOT NULL COMMENT '操作bucket、文件key',
   `is_delete` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除',
   PRIMARY KEY (`id`,`user_id`),
   KEY `key_user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表，存储key等' AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='用户表，存储key等' AUTO_INCREMENT=2 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
