a6cfb8a299a51c781c989014560522bee210fe72
方旭 <fangxu02@meituan.com>
2016-04-27 00:22:33 +0800

    图片裁剪改jq版本,完成度70%

--- application/controllers/ImageTest.php
+++ application/controllers/ImageTest.php
@@ -14,8 +14,15 @@ class ImageTest extends CI_Controller
         $this->load->library('template');
     }
 
+    public function jqTest(){
+        $this->template->display("test/jq_image_upload_demo.html");
+    }
     public function test(){
         $this->template->display("test/image_test.html");
+    }
+
+    public function tt(){
+        $this->template->display("test/test.html");
     }
 
 }
