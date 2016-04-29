9eb0e647ef0d4116996f6a9ceb204afb78516eed
方旭 <fangxu02@meituan.com>
2016-04-29 23:34:24 +0800

    upload image in cavans

--- application/controllers/ImageTest.php
+++ application/controllers/ImageTest.php
@@ -25,4 +25,16 @@ class ImageTest extends CI_Controller
         $this->template->display("test/test.html");
     }
 
+    public function uploadImage(){
+        if($_POST){
+            $imgDecoded = $this->input->post('imgDecoded');
+            $img = str_replace('','+', $imgDecoded);
+            $img = str_replace('data:image/png;base64,', '', $img);
+            $img = base64_decode($img);
+            $f = fopen('/Users/bricks/Desktop/png/test.png', 'w+');
+            fwrite($f,$img);
+            fclose($f);
+        }
+    }
+
 }
